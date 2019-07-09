c9dfb0dfd2dfe0ce1a0e890d9476d1d4acdc9571
nakaikento <okamoto2okamoto@gmail.com>
2019-07-09 12:20:12 +0900

    added admin

--- your_shelf/config/settings.py
+++ your_shelf/config/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- your_shelf/users/models.py
+++ your_shelf/users/models.py
@@ -11,6 +11,7 @@ class User(models.Model):
 
     # フィールドを定義
     name = models.CharField(verbose_name='名前', max_length=255, unique=True)
+    photo = models.ImageField(verbose_name='フォト', null=True, blank=True, upload_to='images/users/')
     books = models.ForeignKey(Book, verbose_name='本', null=True, on_delete=models.CASCADE)
     email = models.EmailField(verbose_name='Eメールアドレス', blank=True)
     password = models.CharField(verbose_name='パスワード', max_length=128)
